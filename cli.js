e48a710b3e165f041643d692405456f63cb14f82
Dolores <alyciahathaways@gmail.com>
2020-07-20 09:49:59 +0800

    完成所有功能

--- cli.js
+++ cli.js
@@ -1,25 +1,26 @@
+#!/usr/bin/env node
 const program = require('commander')
 const api = require('./index.js')
-
-program
-    .option('-x, --xxx', 'what is the x')
 
 program
     .command('add')
     .description('add a task')
     .action((...args) => {
         const words = args[0].args.join(' ')
-        api.add(words)
+        api.add(words).then(() => { console.log('添加成功') }, () => { console.log('添加失败') })
     })
 
 program
     .command('clear')
     .description('clear all tasks')
     .action(() => {
-        api.clear()
+        api.clear().then(() => { console.log('清除成功') }, () => { console.log('清除失败') })
     })
 
+if (process.argv.length === 2) {
+    // 说明用户直接运行 node cli.js
+    api.showAll()
+    return
+}
 
 program.parse(process.argv)
-
-console.log(process.argv)
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const db = require('./db.js')
+const inquirer = require('inquirer')
 
 // 面向接口编程
 module.exports.add = async (title) => {
@@ -10,6 +11,80 @@ module.exports.add = async (title) => {
     await db.write(list)
 }
 
-module.exports.clear = async ()=> {
+module.exports.clear = async () => {
     await db.write([])
+}
+
+module.exports.showAll = async () => {
+    // 读取之前的任务
+    const list = await db.read()
+    // 打印之前的任务
+    inquirer
+        .prompt({
+            type: 'list',
+            name: 'index',
+            message: '请选择你想要操作的任务',
+            choices: [{ name: '退出', value: '-1' }, { name: '+ 创建任务', value: '-2' }].concat(list.map((task, index) => {
+                return { name: `${task.done ? '[x]' : '[_]'} ${index + 1} - ${task.title}`, value: index.toString() }
+            }))
+        })
+        .then(answer => {
+            const index = parseInt(answer.index)
+            if (index >= 0) {
+                // 选中了一个任务
+                inquirer
+                    .prompt({
+                        type: 'list',
+                        name: 'action',
+                        message: '请选择操作',
+                        choices: [
+                            { name: '退出', value: 'quit' },
+                            { name: '已完成', value: 'markAsDone' },
+                            { name: '未完成', value: 'markAsUndone' },
+                            { name: '改标题', value: 'updateTitle' },
+                            { name: '删除', value: 'remove' }
+                        ]
+                    })
+                    .then(answer2 => {
+                        switch (answer2.action) {
+                            case 'markAsDone':
+                                list[index].done = true
+                                db.write(list)
+                                break
+                            case 'markAsUndone':
+                                list[index].done = false
+                                db.write(list)
+                                break
+                            case 'updateTitle':
+                                inquirer.prompt({
+                                    type: 'input',
+                                    name: 'title',
+                                    message: '新的标题',
+                                    default: list[index].title
+                                }).then(answer3 => {
+                                    list[index].title = answer3.title
+                                    db.write(list)
+                                })
+                                break
+                            case 'remove':
+                                list.splice(index, 1)
+                                db.write(list)
+                                break
+                        }
+                    })
+            } else if (index === -2) {
+                // 创建任务
+                inquirer.prompt({
+                    type: 'input',
+                    name: 'title',
+                    message: '请输入任务标题'
+                }).then(answer => {
+                    list.push({
+                        title: answer.title,
+                        done: false
+                    })
+                    db.write(list)
+                })
+            }
+        })
 }
